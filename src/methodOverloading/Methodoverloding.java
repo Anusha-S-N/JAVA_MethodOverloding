4faa8fb36eeeb253930f793d42b54ff13d8a9ba7
Ansha_16 <78400222+Anusha-S-N@users.noreply.github.com>
2023-09-03 21:40:57 +0530

    Add files via upload

---
+++ src/methodOverloading/Methodoverloding.java
@@ -0,0 +1,163 @@
+package methodOverloading;
+
+public class Methodoverloding {
+	
+	
+ public static void main(String[] args) {
+	 
+	 
+	 System.out.println(""+)
+	 public static String getstring(int sec)
+	 {
+		return ""; 
+				
+			
+		}
+	 }
+	 public static String getstring(int min,int sec)
+	 {
+		return "";
+			
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+ }
+
+//	    	System.out.println("HEight in inches:"+ConvertToCentimeters(8));
+//	    	System.out.println("HEight in feet & inches:"+ConvertToCentimeters(5,8));
+//	    }
+//	    	
+//	    	
+//	    	public static double ConvertToCentimeters(int inch)
+//	    	{
+//	    		return inch*2.54;
+//	    	
+//	    	
+//	    	}
+//	    	
+ 
+//	    
+//	    	public static double ConvertToCentimeters(int feet,int inch)
+//	    	{
+//	    		return ((feet*12 )+inch) *2.54;
+//	    	} 	
+//	    
+//	}
+
+	    
+	
+	
+//System.out.println("New score is " + calculateScore("Tim", 500));
+//System.out.println("New score is " + calculateScore(10));
+//}
+//
+//public static int calculateScore(String playerName, int score) {
+//
+//System.out.println("Player " + playerName + " scored " + score + " points");
+//return score * 1000;
+//}
+//
+//public static int calculateScore(int score) {
+//
+//return calculateScore("Anonymous", score);
+//}
+//
+//public static int calculateScore() {
+//
+//System.out.println("No player name, no player score.");
+//return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+//	public static void main(String[] args)
+//	{
+////		int res1=calculatescore(10,34);
+////		System.out.println(""+res1);
+////		int res2=calculatescore(45f,34);
+////		System.out.println(""+res2);
+//		System.out.println("new score is"+calculatescore("HELLO",3));
+//		
+//	}
+//	
+////	public static  int calculatescore(int a,int b)
+////	{
+////		int res = a+b;
+//////		System.out.println(""+res);
+////		return res ;
+////	}
+////	
+////	public static  int calculatescore(float a,int b)
+////	{
+////		int add = (int)a-b;
+//////		System.out.println(""+res);
+////		return  add;
+////	}
+//
+//	public static  int calculatescore(String a,int b)
+//	{ 
+//		return calculatescore("Hi",3);
+//	}
+//}
